f1d6097fb5a52969a08d3aa8e9e3aa11d0534c7e
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-09-08 11:26:18 +0300

    Tracking

--- Guess_Game/target/debug/deps/Guess_Game-7f7c3c9ff6da771e.d
+++
@@ -1,5 +0,0 @@
-/Users/mahmoudramadan/Desktop/rust-Workspace/Rust-WS/Guess_Game/target/debug/deps/libGuess_Game-7f7c3c9ff6da771e.rmeta: src/main.rs
-
-/Users/mahmoudramadan/Desktop/rust-Workspace/Rust-WS/Guess_Game/target/debug/deps/Guess_Game-7f7c3c9ff6da771e.d: src/main.rs
-
-src/main.rs:
--- Guess_Game/target/debug/deps/Guess_Game-eb04db4777df6398.d
+++
@@ -1,5 +0,0 @@
-/Users/mahmoudramadan/Desktop/rust-Workspace/Rust-WS/Guess_Game/target/debug/deps/libGuess_Game-eb04db4777df6398.rmeta: src/main.rs
-
-/Users/mahmoudramadan/Desktop/rust-Workspace/Rust-WS/Guess_Game/target/debug/deps/Guess_Game-eb04db4777df6398.d: src/main.rs
-
-src/main.rs:
