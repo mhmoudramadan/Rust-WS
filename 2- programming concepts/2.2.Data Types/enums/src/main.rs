222485ea3db91f609bffb18f65c4ca389ba3d180
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-09-10 03:33:53 +0300

    Enumeration v1.0.0

--- 2- programming concepts/2.2.Data Types/enums/src/main.rs
+++ 2- programming concepts/2.2.Data Types/enums/src/main.rs
@@ -1,3 +1,24 @@
+
+// enumerations
+// are a way to define a type by enumerating its possible variants. 
+// Each variant can either be simple or hold data, 
+// making enums powerful for representing various states or configurations in a concise way.
+
+//  basic Enum
+// A simple enum without associated data. Each variant is a distinct value.
+
+#[derive(Debug)]
+enum Week{
+    saturday,
+    sunday,
+    monday,
+    tuesday,
+    wednesay,
+    thursday,
+    friday,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let day_ = Week::saturday;
+    println!("{:?}",day_);
 }
