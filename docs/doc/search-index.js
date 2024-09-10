89aa77fab1441b9bec00171c9f067802d6fce587
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-09-10 03:13:23 +0300

    docmentation v1.0.0

---
+++ docs/doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["lib",{"t":"H","n":["add"],"q":[[0,"lib"]],"i":[0],"f":"{{bb}b}","D":"`","p":[[1,"u64"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAEAEAAAAAAAAQA="}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
