c0df2a482715adbb1798bd6f734426a3ff795db4
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-09-15 12:40:14 +0300

    foc-while-v1.1.0

--- 3- flow of control/3.3.while/foc-while/src/main.rs
+++ 3- flow of control/3.3.while/foc-while/src/main.rs
@@ -1,3 +1,15 @@
+/// while keyword can be used to run a loop while a condition is true.
+/// Repeats as long as a condition is true.
+/// typically used when the number of iterations is not known beforehand
+///  
+
 fn main() {
-    println!("Hello, world!");
+   /// The code snippet is using a `while` loop in Rust to decrement the value of the variable `num`
+   /// from 15 to -2.
+    let mut num = 15i8;
+
+    while num != -2 {
+        println!("Number is {}", num);
+        num -=1;
+    }
 }
