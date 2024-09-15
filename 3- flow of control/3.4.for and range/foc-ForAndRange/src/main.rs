e9cc5d08ef778db07146992253cdf309b4d5544e
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-09-15 12:57:01 +0300

    foc-fr-v1.1.0

--- 3- flow of control/3.4.for and range/foc-ForAndRange/src/main.rs
+++ 3- flow of control/3.4.for and range/foc-ForAndRange/src/main.rs
@@ -1,3 +1,22 @@
+/// for in  range construct can be used to iterate through an Iterator
+/// One of the easiest ways to create an iterator is to use the range notation a..b
+/// This yields values from a (inclusive) 
+///                    to   b (exclusive) in steps of one.
+/// 
+///  
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    // for and range
+    /// This code snippet is using a `for` loop with a range to iterate from 1 (inclusive) to 100
+    /// (exclusive).
+    for n in 1..100 {
+        if n % 10 == 0 {
+            println!("Valid division Number is {}",n);
+        } else {
+            println!("Invalid division is {}",n)
+        }
+    }
 }
