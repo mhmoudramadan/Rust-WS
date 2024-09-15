11fe76bc2597cd102b6eb791dee8df7fa5c0877c
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-09-15 14:27:44 +0300

    for-fr-v1.2.0

--- 3- flow of control/3.4.for and range/foc-ForAndRange/src/main.rs
+++ 3- flow of control/3.4.for and range/foc-ForAndRange/src/main.rs
@@ -19,4 +19,12 @@ fn main() {
             println!("Invalid division is {}",n)
         }
     }
+
+  /// The code snippet `for n in 1..=100 { println!("N is {}", n); }` is using a `for` loop with a range
+  /// to iterate from 1 (inclusive) to 100 (inclusive). This means that the loop will iterate over
+  /// values starting from 1 up to and including 100. Inside the loop, it will print out the value of
+  /// `n` with the message "N is [value]".
+    for n in 1..=100 {
+        println!("N is {}",n);
+    }
 }
