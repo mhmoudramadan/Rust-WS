d0230e1894353c2ec24218c3158624c6c13f7543
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-09-16 15:00:43 +0300

    foc-match-bs-v1.1.0

--- 3- flow of control/3.5.match/foc-match/src/main.rs
+++ 3- flow of control/3.5.match/foc-match/src/main.rs
@@ -1,3 +1,22 @@
+/// match keyword
+/// used for pattern matching, providing a powerful control flow construct that can be used to match values
+/// against patterns and run code based on which pattern is matched. 
+/// It is similar to a switch statement in other languages, but much more powerful and flexible.
+
+
 fn main() {
-    println!("Hello, world!");
+
+    // Basic syntax
+    // 
+    // . [ _ . ] pattern acts as a catch-all for any value not explicitly handled
+    //  by other patterns (similar to the default case in a switch statement).
+    
+    let num =3u8;
+    match num {
+        1 => println!("number is 1"),
+        2 => println!("number is 2"),
+        3 => println!("number is 3"),
+        _ => println!("number is 1"),
+    }
+    
 }
