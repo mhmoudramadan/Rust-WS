c8c794041e898e159f495184f98c21a2632b5b32
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-09-16 15:05:03 +0300

    foc-match-MP-v1.3.0

--- 3- flow of control/3.5.match/foc-match/src/main.rs
+++ 3- flow of control/3.5.match/foc-match/src/main.rs
@@ -6,17 +6,29 @@
 
 fn main() {
 
-    // Basic syntax
+    // ----------------- Basic syntax ----------------
     // 
-    // . [ _ . ] pattern acts as a catch-all for any value not explicitly handled
+    // . [ _ ] pattern acts as a catch-all for any value not explicitly handled
     //  by other patterns (similar to the default case in a switch statement).
-    
-    let num =3u8;
+
+    let mut num = 3u8;
     match num {
         1 => println!("number is 1"),
         2 => println!("number is 2"),
         3 => println!("number is 3"),
-        _ => println!("number is 1"),
+        _ => println!("this is default Case like switch statement"),
+    }
+
+    // -----------------  Multpile patterns ---------
+
+
+   /// The code snippet `num = 15; match num { 1 | 2 | 3 => println!("number is 1 or 2 or 3"), _ =>
+   /// println!("this is default Case like switch statement"), }` is using the `match` keyword in Rust
+   /// to perform pattern matching on the value of the variable `num`.
+    num = 15;
+    match num {
+        1 | 2 | 3 => println!("number is 1 or 2 or 3"),
+        _ => println!("this is default Case like switch statement"),
     }
     
 }
