0fe58498dae49bd5e1ef6d1e12a29de9a021d4f0
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-09-22 12:28:01 +0300

    fun-HOF-v1.1.0

--- 4- function/4.4.higher-order-functions/higher-order-fun/src/main.rs
+++ 4- function/4.4.higher-order-functions/higher-order-fun/src/main.rs
@@ -1,3 +1,32 @@
+//  HOF
+// functions that either take one or more functions as arguments or return a function
+// Key Points:
+//  Higher-order functions take other functions or closures as parameters or return them.
+//  You can pass functions or closures as arguments to functions, and return them as results from functions.
+
+
+// Passing a Function as an Argument
+
+// Simple fun
+fn add(x:u32,y:u32) ->u32 {
+    x+y
+}
+
+// HOF
+fn operation<F>(f:F,z:u32,c:u32) -> u32
+    where
+    F:Fn(u32,u32)->u32, 
+    {
+
+        // Call the passed function
+        f(z,c)
+    }
+    
+
+
 fn main() {
-    println!("Hello, world!");
+    // Pass `add` function to `operation`
+    let result = operation(add, 30, 10); 
+    println!("Result: {}", result);
+    
 }
