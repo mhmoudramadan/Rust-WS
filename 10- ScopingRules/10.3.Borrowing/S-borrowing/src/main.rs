9a2462f9ae24b7ddbdcbee40c0ac427f11c1e00f
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-09-29 17:16:29 +0300

    scoping-S-mutborrow-v1.2.0

--- 10- ScopingRules/10.3.Borrowing/S-borrowing/src/main.rs
+++ 10- ScopingRules/10.3.Borrowing/S-borrowing/src/main.rs
@@ -9,7 +9,7 @@
 // * 2- mutable (&mut T)
 // This mechanism ensures safe memory access by controlling how values are accessed or modified.
 
-// !! mmutable Borrowing
+// !! immutable Borrowing
 // An immutable reference allows you to read data without taking ownership.
 //  Multiple immutable references can exist at the same time, but they cannot modify the data.
 
@@ -17,10 +17,28 @@ fn display(s:&String) {
     println!("{}",s);
 }
 
+// !! Mutable Borrowing
+// * A mutable reference allows you to modify the borrowed value. 
+// * only one mutable reference can exist at a time to prevent data races and ensure memory safety.
+
+fn modify_value(s:&mut String){
+    s.push_str(".borrow");
+}
+
 fn main() {
     println!("Hello, Borrowing!");
 
+    // ! immutable Borrowing
     let s1 = String::from("immutable Borrowing");
     display(&s1); // * // Borrow `s1` immutably
     println!("S1 value is still valid {}",s1);
+
+
+    // ! mutable Borrowing
+    let mut s2 = String::from("mutable ");
+    modify_value(&mut s2);
+    println!("modified valued after mutable borrowing is {}",s2);
+
+
+
 }
