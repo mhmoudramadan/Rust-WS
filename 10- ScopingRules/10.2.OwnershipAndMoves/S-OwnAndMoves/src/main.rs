0ef3008fb4d6c117e368a421df25283f0bd7cbed
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-09-29 16:06:05 +0300

    scoping-S-move-v1.1.0

--- 10- ScopingRules/10.2.OwnershipAndMoves/S-OwnAndMoves/src/main.rs
+++ 10- ScopingRules/10.2.OwnershipAndMoves/S-OwnAndMoves/src/main.rs
@@ -7,10 +7,14 @@
 
 
 
-
-//* Each value in Rust has a variable that’s called its owner.
-/// At any given time, there can be only one owner for a value. 
-/// When the owner goes out of scope, the value is dropped, and the memory is freed.
+// !! Ownership
+//*  Each value in Rust has a variable that’s called its owner.
+//*  At any given time, there can be only one owner for a value. 
+//*  When the owner goes out of scope, the value is dropped, and the memory is freed.
+ 
+// !! Moves in Rust
+// * When a value is moved, the ownership of the value is transferred from one variable to another. 
+// * After a move the original variable can no longer be used because it no longer owns the data
 
 fn main() {
     println!("Hello, Ownership and Moves!");
@@ -20,4 +24,15 @@ fn main() {
         let my_str = String::from("Hello");
         println!("my string is {}",my_str);
     } // my_str goes out of scope and the memory for the String is freed
+
+    // ! Move in rust 
+
+    let s1 : &str = "mahmoud";
+    // ?? before moved
+     println!("{}", s1); 
+    //  !! Move here
+    let s2 = s1; // s1 is moved to s2
+
+    // println!("{}", s1); // This would cause an error because s1 is no longer valid
+    println!("{}", s2); // s2 now owns the String
 }
