cbe2108a15c104a9a4a948968fdacec6403bcbe7
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-10-01 15:40:32 +0300

    T-Intro-v1.2.0

--- 11- traits/11.1.intro/T-intro/src/main.rs
+++ 11- traits/11.1.intro/T-intro/src/main.rs
@@ -1,3 +1,31 @@
+// !! Traits
+// * is a collection of methods defined for an unknown type: Self. They can access other methods declared in the same trait
+// * traits define shared behavior between types, similar to interfaces in other languages. 
+// * You can implement traits for various types, including structs, enums, and primitive types.
+// * Traits are used to ensure types share common functionality, allowing polymorphism and code reuse.
+
+
+struct Book {
+    title : String,
+    author: String,
+}
+
+trait Describable {
+    fn describe(&self)->String;
+}
+
+impl Describable for Book {
+    fn describe(&self)->String{
+        format!("{} by {}", self.title, self.author)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("!!!! Hello, Traits !!!!");
+    let book = Book {
+        title: String::from("The Rust Book Guide"),
+        author: String::from("Mahmoud ramadan"),
+    };
+
+    println!("{}", book.describe());
 }
