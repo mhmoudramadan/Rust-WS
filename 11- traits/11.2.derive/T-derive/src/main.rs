0690a177abeb23338393f6ae5c3f6a622667b0c2
mhmoudramadan <mhmoudramadan111@gmail.com>
2024-10-02 13:13:49 +0300

    T-derive-v1.2.0

--- 11- traits/11.2.derive/T-derive/src/main.rs
+++ 11- traits/11.2.derive/T-derive/src/main.rs
@@ -1,3 +1,17 @@
+// !! derive trait
+// * The compiler is capable of providing basic implementations for some traits via the #[derive] attribute. 
+// * These traits can still be manually implemented if a more complex behavior is required.
+// the derive attribute allows you to automatically implement common traits for your structs and enums. This reduces the need to write repetitive boilerplate code.
+//  The most common traits that can be automatically derived 
+// include 
+// * Debug, 
+// * Clone, 
+// * PartialEq,
+// * Eq, 
+// * PartialOrd
+// * Ord
+// * Hash
+// * and Default.
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, derive trait!");
 }
